52f09b754954552c5c1357d0c9a063808cd0dd55
alexander.graham@mycit.ie <33347630+Graham115@users.noreply.github.com>
2019-11-27 12:28:10 +0000

    Packaged added for repo

--- Project2/src/main/java/ie/group23/RepositoryConfig.java
+++ Project2/src/main/java/ie/group23/RepositoryConfig.java
@@ -1,6 +1,8 @@
 package ie.group23;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 import ie.group23.domain.Users;
 
