d1b8410b1703dc7ff21e1eddd47b8b6d40a5dd9d
alexander.graham@mycit.ie <33347630+Graham115@users.noreply.github.com>
2019-12-04 13:29:14 +0000

    I dont know

--- Project2/src/main/java/ie/group23/DataLoader.java
+++ Project2/src/main/java/ie/group23/DataLoader.java
@@ -17,46 +17,36 @@ public class DataLoader implements ApplicationRunner {
 
 	@Autowired
 	UsersService usersService;
-	
+
 	@Autowired
 	JobService jobService;
-	
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		
-		
-<<<<<<< HEAD
-		Users John = new Users("John", "john@yahoo.com", "bgvrje", "0873657901");
-		Users Mary = new Users("Mary", "mary@hotmail.com", "ehtieh", "0823409283");
-		Users Tom = new Users("Tom", "tom@eircom.net", "gfnklj", "0862954120");
-//		
-=======
+
+
 		Users John = new Users("John", "john@yahoo.com", "bfjkds", "0862546790");
 		Users Mary = new Users("Mary", "mary@hotmail.com", "ehtieh", "0823409283");
 		Users Tom = new Users("Tom", "tom@eircom.net", "gnvrwk", "0875418053");
-		
->>>>>>> User-Database-Populating
+
 		John = usersService.save(John);
 		log.info("LOMBOK TESTING: " + John);
 		Mary = usersService.save(Mary);
 		log.info("LOMBOK TESTING: " + Mary);
 		Tom = usersService.save(Tom);
 		log.info("LOMBOK TESTING: " + Tom);
-<<<<<<< HEAD
-		
+
 		Job job1 = new Job("Bathroom Pluming", "Clogged pipes in the bathroom", "2019-11-15");
 		Job job2 = new Job("Fittings", "Wardrobe to be fitted into the bedroom", "2019-12-05");
 		Job job3 = new Job("Grinds", "Need some help with maths equations", "2019-12-01");
-		
+
 		job1 = jobService.save(job1);
 		log.info("LOMBOK TESTING: " + job1);
 		job2 = jobService.save(job2);
 		log.info("LOMBOK TESTING: " + job2);
 		job3 = jobService.save(job3);
 		log.info("LOMBOK TESTING: " + job3);
-=======
->>>>>>> User-Database-Populating
-		
+
 	}
 
 }
