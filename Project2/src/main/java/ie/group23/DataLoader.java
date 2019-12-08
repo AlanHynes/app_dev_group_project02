384e03a174c8fb088c83baa3a8ffcbcf6104564c
Alan Hynes r00145398 <alan.hynes@mycit.ie>
2019-12-08 20:57:10 +0000

    Update DataLoader.java

--- Project2/src/main/java/ie/group23/DataLoader.java
+++ Project2/src/main/java/ie/group23/DataLoader.java
@@ -1,5 +1,7 @@
 package ie.group23;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -62,12 +64,12 @@ public class DataLoader implements ApplicationRunner {
 		Tom = usersService.save(Tom);
 		log.info("LOMBOK TESTING: " + Tom);
 		
-		Job job1 = new Job("Bathroom Pluming", "Clogged pipes in the bathroom", "2019-11-15", Mary);
-		Job job2 = new Job("Fittings", "Wardrobe to be fitted into the bedroom", "2019-12-05", John);
-		Job job3 = new Job("Grinds", "Need some help with maths equations", "2019-12-01", Tom);
-		Job job4 = new Job("House work", "New shed to be built", "2015-10-17", Mary);
-		Job job5 = new Job("Phone problem", "Broken vscreen", "2013-09-01", Mary);
-		Job job6 = new Job("Broadband", "Like a new broadband package", "2012-12-06", Mary);
+		Job job1 = new Job("Bathroom Pluming", "Clogged pipes in the bathroom", LocalDate.now().minusDays(20), Mary);
+		Job job2 = new Job("Fittings", "Wardrobe to be fitted into the bedroom", LocalDate.now().minusDays(15), John);
+		Job job3 = new Job("Grinds", "Need some help with maths equations", LocalDate.now().minusDays(22), Tom);
+		Job job4 = new Job("House work", "New shed to be built", LocalDate.now().minusDays(21), Mary);
+		Job job5 = new Job("Phone problem", "Broken vscreen", LocalDate.now().minusDays(5), Mary);
+		Job job6 = new Job("Broadband", "Like a new broadband package", LocalDate.now().minusDays(29), Mary);
 
 		job1 = jobService.save(job1);
 		log.info("LOMBOK TESTING: " + job1);
