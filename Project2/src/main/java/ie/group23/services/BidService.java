18c58d16e894a929b7a19c9e74c33f9bc0f80942
Alan Hynes r00145398 <alan.hynes@mycit.ie>
2019-12-08 23:06:48 +0000

    tried to get bid working

--- Project2/src/main/java/ie/group23/services/BidService.java
+++ Project2/src/main/java/ie/group23/services/BidService.java
@@ -7,5 +7,6 @@ import ie.group23.domain.Bid;
 public interface BidService {
 	List<Bid> listInAlphabeticalOrder();
 	Bid findBid(int bidID);
-	Bid save(Bid bid); 
+	Bid save(Bid bid);
+	Bid updateBid(int jobID, double bidAmount); 
 }
